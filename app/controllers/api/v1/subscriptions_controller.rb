56340671aa6fefc56efaf2fa3d8a96d6bc248cc4
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-19 16:11:41 -0600

    Add controller update action for updating subscriptions

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SubscriptionsController < ApplicationController
   def create
     customer = Customer.find(params[:customer_id])
-    subscription = customer.subscriptions.create!(JSON.parse(params[:params]))
+    subscription = customer.subscriptions.create!(subscription_params)
 
     render json: {
       "data": {
@@ -17,5 +17,30 @@ class Api::V1::SubscriptionsController < ApplicationController
         }
       }
     }, status: :created
+  end
+
+  def update
+    subscription = Subscription.find(params[:id])
+    subscription.update!(subscription_params)
+
+    render json: {
+      "data": {
+        "id": subscription.id,
+        "type": "subscription",
+        "attributes": {
+          title: subscription.title,
+          price: subscription.price,
+          status: subscription.status,
+          frequency: subscription.frequency,
+          tea_id: subscription.tea_id,
+          customer_id: subscription.customer_id
+        }
+      }
+    }, status: :created
+  end
+
+  private
+  def subscription_params
+    params.permit(:title, :price, :status, :frequency, :tea_id, :customer_id)
   end
 end
