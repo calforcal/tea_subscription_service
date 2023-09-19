2bcfd47ec10d6bf46c4ad8d502f0b0a648c124aa
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-19 15:06:59 -0600

    Update Subscription model to accept enums:

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,4 +3,6 @@ class Subscription < ApplicationRecord
 
   belongs_to :customer
   belongs_to :tea
+
+  enum frequency: { weekly: 0, monthly: 1, bi_monthly: 2, quarterly: 3, semi_annually: 4, annually: 5}
 end
