a81c24ee2b506f32c2c7fe814b5eaa967e6e5295
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-19 15:06:27 -0600

    Update DB to accept enums for Subscriptions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_130640) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_203907) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +27,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_130640) do
     t.string "title"
     t.float "price"
     t.boolean "status"
-    t.string "frequency"
+    t.integer "frequency"
     t.bigint "customer_id", null: false
     t.bigint "tea_id", null: false
     t.datetime "created_at", null: false
