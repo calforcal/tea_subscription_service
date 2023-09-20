33e60eb6d085dc1b5971d5ba6ddbe23496044413
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-09-19 19:02:18 -0600

    Refactor controllers to include find_customer method

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::SubscriptionsController < ApplicationController
+  before_action :find_customer
+
   def index
-    customer = Customer.find(params[:customer_id])
-    subscriptions = customer.subscriptions
+    subscriptions = @customer.subscriptions
     render json: SubscriptionSerializer.new(subscriptions).serialize_multiple, status: 200
   end
 
   def create
-    customer = Customer.find(params[:customer_id])
-    subscription = customer.subscriptions.create!(subscription_params)
+    subscription = @customer.subscriptions.create!(subscription_params)
     render json: SubscriptionSerializer.new(subscription).serialize_one, status: :created
   end
 
@@ -20,5 +20,9 @@ class Api::V1::SubscriptionsController < ApplicationController
   private
   def subscription_params
     params.permit(:title, :price, :status, :frequency, :tea_id, :customer_id)
+  end
+
+  def find_customer
+    @customer = Customer.find(params[:customer_id])
   end
 end
